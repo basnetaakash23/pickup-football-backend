5ec94b8dddb2f36ae91ae231d127202144f94474
basnetaakash23 <aakash.basnet@bison.howard.edu>
2025-05-22 12:53:58 -0500

    "Integrated email services with kafka"

---
+++ src/main/java/com/football/pickup/games/dto/request/EmailRequestDto.java
@@ -0,0 +1,26 @@
+package com.football.pickup.games.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Slf4j
+public class EmailRequestDto {
+
+    private String to;
+    private String subject;
+    private String message;
+
+    @Override
+    public String toString() {
+        return "EmailRequest{" +
+                "to='" + to + '\'' +
+                ", subject='" + subject + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/football/pickup/games/service/EmailEventProducer.java
@@ -0,0 +1,23 @@
+package com.football.pickup.games.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.football.pickup.games.dto.request.EmailRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailEventProducer {
+
+    private final KafkaTemplate<Object, String> kafkaTemplate;
+    private static final String TOPIC = "email-topic-2";
+
+    public void sendEmailEvent(EmailRequestDto emailRequest) throws JsonProcessingException {
+        kafkaTemplate.send(TOPIC, new ObjectMapper().writeValueAsString(emailRequest));
+
+    }
+
+
+}
